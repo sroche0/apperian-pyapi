ad87a4f81adc6a2d84609f0c5cbb996de91429ec
sroche0 <sroche@apperian.com>
2015-06-24 16:50:29 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/pypa/sampleproject',
+    url='https://github.com/sroche0/apperian-pyapi/',
 
     # Author details
     author='Shawn Roche',
@@ -64,7 +64,7 @@ setup(
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     # packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    packages=['apperian'],
+    packages=['apperian', 'data'],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
@@ -85,14 +85,14 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'endpoints': ['endpoints.json'],
+        'endpoints': ['data/endpoints.json'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('my_data', ['data/endpoints.json'])],
+    # data_files=[('my_data', ['data/endpoints.json'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
