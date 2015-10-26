a9245b8bb97f55e2633555c8615db541d7380557
shawn roche <sroche@apperian.com>
2015-10-26 14:23:28 -0400

    fixed app_toggle() data parameter to be JSON format

--- apperian/apperian.py
+++ apperian/apperian.py
@@ -228,7 +228,7 @@ class Ease:
         :return: Dict of request status
         """
         url = '{}/v1/applications/{}'.format(self.region['Python Web Services'], app_psk)
-        r = self.s.put(url, data={'enabled': state})
+        r = self.s.put(url, data=json.dumps({'enabled': state}))
         result = response_check(r, 'update_application_result')
         return result
 
