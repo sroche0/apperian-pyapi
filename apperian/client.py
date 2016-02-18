b70e7b004f78f7e6bf69731f5a82e7eef4b41bac
shawn roche <sroche@apperian.com>
2016-02-18 13:55:31 -0500

    added assignment for self.app, self.group, and self.user to __init__ for Ease()

--- apperian/client.py
+++ apperian/client.py
@@ -22,7 +22,7 @@ class Ease:
         self.username = user
         self.password = pw
         self.region = {}
-        self.user, self.app, self.group = '','',''
+        self.user, self.app, self.group = '', '', ''
         self.s = requests.Session()
         self.s.headers.update({"Content-Type": "application/json"})
         self.valid = Ease.set_region(self, region)
