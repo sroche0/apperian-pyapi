25b8fda13ee47094ec539f26fb66ce298fa6950a
shawn roche <sroche@apperian.com>
2016-02-18 16:36:18 -0500

    Filled in most of missing functions

--- apperian/modules/applications.py
+++ apperian/modules/applications.py
@@ -6,7 +6,7 @@ from helpers import response_check
 class Apps:
     def __init__(self, session, region):
         self.session = session
-        self.region = region
+        self.base = '{}/v1/applications'.format(region['Python Web Services'])
 
     def list(self):
         """
@@ -16,7 +16,7 @@ class Apps:
 
         :return: Dict with key:value pairs of the app psk and it's metadata. For example: {123:{METADATA}}
         """
-        url = '%s/v1/applications/' % self.region['Python Web Services']
+        url = self.base
         r = self.session.get(url)
         result = response_check(r, 'applications')
         # if result['status'] == 200:
@@ -34,31 +34,36 @@ class Apps:
         :param psk: Unique ID of the app
         :return: Returns dict of metadata about the specified application. Specify the app with app_psk.
         """
-        url = '%s/v1/applications/%s' % (self.region['Python Web Services'], str(psk))
+        url = '{}/{}'.format(self.base, str(psk))
         r = self.session.get(url)
         result = response_check(r, 'application')
         return result
 
-    def add_screenshot(self, psk, slot):
+    def add_screenshot(self, psk, form, slot):
         """
         POST /applications/<app_psk>/screenshots/[phone/tablet]/<slot>
         Upload a Screenshot for An Application
 
         :param psk: unique ID of the app
+        :param form: Form factor of the app the screenshot it for. Valid Value: phone, tablet
         :param slot: Order in which the screenshot will display on the app's details page in the App Catalog. If you
             specify the same slot of a screenshot that is already stored for the same form factor (phone or tablet),
             the new file will overwrite the previously added file. Valid values: 1-5:
         :return:
 
         """
-        pass
+        url = '{}/{}/screenshots/{}/{}'.format(self.base, psk, form, slot)
+        r = self.session.post(url)
+        result = response_check(r)
+        return result
 
-    def delete_screenshot(self, psk, slot):
+    def delete_screenshot(self, psk, form, slot):
         """
         DELETE /applications/<app_psk>/screenshots/[phone/tablet]/<slot>
         Delete an Application Screenshot
 
         :param psk: unique ID of the app
+        :param form: Form factor of the app the screenshot it for. Valid Value: phone, tablet
         :param slot: Specify the slot of the screenshot you want to delete. You can view a list with slot number of all
             the screenshots stored for an app by returning details about the app with the GET /applications/<app_psk>
             resource. Be sure to specify the correct form factor (phone or tablet) in the URL. There is a slot 1 - 5
@@ -66,7 +71,10 @@ class Apps:
         :return:
 
         """
-        pass
+        url = '{}/{}/screenshots/{}/{}'.format(self.base, psk, form, slot)
+        r = self.session.delete(url)
+        result = response_check(r)
+        return result
 
     def get_media(self, psk):
         """
@@ -74,9 +82,12 @@ class Apps:
         List data for a specific app
 
         :param psk: Unique ID of the app
-        :return: Returns dict of media files and their metadata.
+        :return: Returns list of media files and their metadata in dicts.
         """
-        pass
+        url = '{}/{}/related_media'.format(self.base, str(psk))
+        r = self.session.get(url)
+        result = response_check(r, 'related_media')
+        return result
 
     def get_usage(self, psk, start_date, end_date):
         """
@@ -88,8 +99,7 @@ class Apps:
         :param end_date: End date for the statistical period. yyyy-mm-dd format
         :return:Returns Download Count and Usage Count for an application during a specified statistical time period.
         """
-        url = '%s/v1/applications/%s/stats?start_date=%s&end_date=%s' % \
-              (self.region['Python Web Services'], str(psk), start_date, end_date)
+        url = '{}/{}/stats?start_date={}&end_date={}'.format(self.base, psk, start_date, end_date)
         r = self.session.get(url)
         result = response_check(r, 'app_stats')
         return result
@@ -112,7 +122,7 @@ class Apps:
 
         :return: Returns data about all the App Catalogs in the authenticated user's organization
         """
-        url = '%s/v1/applications/app_catalogs/' % self.region['Python Web Services']
+        url = '{}/app_catalogs/'.format(self.base)
         r = self.session.get(url)
         result = response_check(r, 'app_catalogs')
         if result['status'] == 200:
@@ -131,7 +141,7 @@ class Apps:
         :return: Dict with key:value pairs of the app psk and it's metadata. For example: {123:{METADATA}}
 
         """
-        url = '%s/v1/applications/user' % self.region['Python Web Services']
+        url = '{}/user'.format(self.base)
         r = self.session.get(url)
         result = response_check(r, 'applications')
         if result['status'] == 200:
@@ -161,17 +171,20 @@ class Apps:
         :param state: Boolean Value of desired state of the app
         :return: Dict of request status
         """
-        url = '{}/v1/applications/{}'.format(self.region['Python Web Services'], app_psk)
+        url = '{}/v1/applications/{}'.format(self.base, app_psk)
         r = self.session.put(url, data=json.dumps({'enabled': state}))
         result = response_check(r, 'update_application_result')
         return result
 
     def delete(self, psk):
         """
-         DELETE /applications/<app_psk>
+        DELETE /applications/<app_psk>
         Deletes a specific application from your EASE organization
 
         :param psk: Unique ID for the app
         :return: string 'success' on success, dict of error on failure
         """
-        pass
+        url = '{}/{}'.format(self.base, psk)
+        r = self.session.delete(url)
+        result = response_check(r, 'deleted_application')
+        return result
