4147d02f70a384bfce4b4e272e3f4c32eff1f96a
shawn roche <sroche@apperian.com>
2016-03-28 13:04:53 -0400

    trying to reconfig setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
-__author__ = 'sroche'
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
-
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-# To use a consistent encoding
 from codecs import open
 from os import path
+__author__ = 'sroche'
+
 
 here = path.abspath(path.dirname(__file__))
 
@@ -19,25 +12,13 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 
 setup(
     name='apperian',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0',
-
+    version='0.1.0',
     description='Python module for interacting with the EASE APIs',
     long_description=long_description,
-
-    # The project's main homepage.
     url='https://github.com/sroche0/apperian-pyapi/',
-
-    # Author details
     author='Shawn Roche',
     author_email='sroche@apperian.com',
-
-    # Choose your license
     license='MIT',
-
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         # How mature is this project? Common values are
@@ -59,12 +40,13 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='apperian ease api',
+    keywords=['apperian', 'ease'],
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     # packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    packages=find_packages(),
+    # packages=find_packages(),
+    packages=['apperian', 'modules', 'endpoints'],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
@@ -85,21 +67,8 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'apperian': ['modules/*.py', 'endpoints.json'],
-    },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place modules files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('bin/apperian_data', ['modules/endpoints.json'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-        'console_scripts': [
-            'sample=sample:main',
-        ],
-    },
+        'apperian': 'apperian',
+        'modules': 'apperian/modules/*.py',
+        'endpoints': 'endpoints.json'
+    }
 )
